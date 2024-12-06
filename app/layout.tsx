4dc631e632962858eb17a565701815eeac8eacbd
Iqbal Shah <iqbalshahandiri786@gmail.com>
2024-12-06 23:58:44 +0500

    Set up Light & Dark Theme , Shadcn and Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import React from "react";
+
 import "./globals.css";
+import Navbar from "@/components/navigation/navbar";
+
+import ThemeProvider from "../context/Theme";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,11 +33,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute={"class"}
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
